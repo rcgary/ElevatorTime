1f6c75a197d0b9d7248a827f776ac3315597ce2e
Chao Ruan <chaoruan818@gmail.com>
2016-03-01 13:29:09 +1100

    Move spec for elevator

--- ElevatorTimeTests/ElevatorSpec.swift
+++ ElevatorTimeTests/ElevatorSpec.swift
@@ -16,6 +16,18 @@ class ElevatorSpec: QuickSpec {
                 expect(elevator.destinations.count) == 0
                 expect(elevator.direction.rawValue) == Direction.Stationary.rawValue
             }
+            
+            it("should move the elevator") {
+                var elevator = Elevator(id: "A", currentLvl: 0)
+                expect(elevator.currentLvl) == 0
+                elevator.move(2)
+                expect(elevator.currentLvl) == 2
+                expect(elevator.direction.rawValue) == Direction.Up.rawValue
+                elevator.move(1)
+                expect(elevator.currentLvl) == 1
+                expect(elevator.direction.rawValue) == Direction.Down.rawValue
+
+            }
         }
     }
 }
